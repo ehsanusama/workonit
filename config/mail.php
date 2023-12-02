971321fe700be73d6dc5f450898b577c8d48427b
Usama Ehsan <124584350+ehsanusama@users.noreply.github.com>
2023-12-02 18:52:57 +0500

    final commit

--- config/mail.php
+++ config/mail.php
@@ -36,14 +36,17 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'driver' => env('MAIL_DRIVER', 'smtp'),
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
             'port' => env('MAIL_PORT', 587),
+            'from' => [
+                'address' => env('MAIL_FROM_ADDRESS', "usamaa.ehsan@gmail.com"),
+                'name' => env('MAIL_FROM_NAME', 'Developer'),
+            ],
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN'),
+            'username' => env('MAIL_USERNAME', 'usamaa.ehsan@gmail.com'),
+            'password' => env('MAIL_PASSWORD', '*do it if ucan#'),
+
         ],
 
         'ses' => [
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SignupController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +17,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+Route::get('/login', [LoginController::class, "index"]);
+Route::post('/login', [LoginController::class, "store"])->name("login");
+Route::get('/signup', [SignupController::class, "index"]);
+Route::post('/signup', [SignupController::class, "store"]);
+Route::get('/logout', [DashboardController::class, "logout"]);
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, "index"]);
 });
