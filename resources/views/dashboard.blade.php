712a33aa32b9f100deae6a9056efaba654237404
Usama Ehsan <124584350+ehsanusama@users.noreply.github.com>
2023-12-02 18:54:17 +0500

    web

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,6 +12,8 @@
 
 <body>
     <div class="background-green"></div>
+
+
     <div class="main-container">
         <div class="left-container">
 
@@ -20,12 +22,12 @@
                 <div class="user-img">
                     <img class="dp" src="https://www.codewithfaraz.com/InstaPic.png" alt="">
                 </div>
-                {{-- <div class="nav-icons">
+                <div class="nav-icons">
                     <li><i class="fa-solid fa-users"></i></li>
                     <li><i class="fa-solid fa-message">
                         </i></li>
                     <li><i class="fa-solid fa-ellipsis-vertical"></i></li>
-                </div> --}}
+                </div>
             </div>
 
 
@@ -51,7 +53,8 @@
 
             <!--chats -->
             <div class="chat-list">
-                <div class="chat-box">
+                <div class="chat-box"
+                    title="https://www.uchat.com.au/js/widget/ve4cwaxxkhqj6ivg/embed.js?id=embed_chatbot_container_id">
                     <div class="img-box">
                         <img class="img-cover"
                             src="https://lh5.googleusercontent.com/-7ssjf_mDE1Q/AAAAAAAAAAI/AAAAAAAAASo/tioYx2oklWEHoo5nAEyCT-KeLxYqE5PuQCLcDEAE/s100-c-k-no-mo/photo.jpg"
@@ -59,7 +62,7 @@
                     </div>
                     <div class="chat-details">
                         <div class="text-head">
-                            <h4>Nowfal</h4>
+                            <h4>CGit Support</h4>
                             <p class="time unread">11:49</p>
                         </div>
                         <div class="text-message">
@@ -68,121 +71,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="chat-box">
-                    <div class="img-box">
-                        <img class="img-cover"
-                            src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                            alt="">
-                    </div>
-                    <div class="chat-details">
-                        <div class="text-head">
-                            <h4>Rohan</h4>
-                            <p class="time unread">10:49</p>
-                        </div>
-                        <div class="text-message">
-                            <p>“I’ll be there.”</p>
-                            <b>4</b>
-                        </div>
-                    </div>
-                </div>
-                <div class="chat-box">
-                    <div class="img-box">
-                        <img class="img-cover"
-                            src="https://images.pexels.com/photos/8367221/pexels-photo-8367221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                            alt="">
-                    </div>
-                    <div class="chat-details">
-                        <div class="text-head">
-                            <h4>Zoya</h4>
-                            <p class="time unread">09:49</p>
-                        </div>
-                        <div class="text-message">
-                            <p>“Make somebody’s day.”</p>
-                            <b>2</b>
-                        </div>
-                    </div>
-                </div>
-                <div class="chat-box">
-                    <div class="img-box">
-                        <img class="img-cover"
-                            src="https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                            alt="">
-                    </div>
-                    <div class="chat-details">
-                        <div class="text-head">
-                            <h4>Ava</h4>
-                            <p class="time">08:49</p>
-                        </div>
-                        <div class="text-message">
-                            <p>“Dreams come true.”</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="chat-box active">
-                    <div class="img-box">
-                        <img class="img-cover"
-                            src="https://images.pexels.com/photos/2474307/pexels-photo-2474307.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                            alt="">
-                    </div>
-                    <div class="chat-details">
-                        <div class="text-head">
-                            <h4>Leo</h4>
-                            <p class="time">07:49</p>
-                        </div>
-                        <div class="text-message">
-                            <p>Awesome! I'll start a vid..</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="chat-box">
-                    <div class="img-box">
-                        <img class="img-cover"
-                            src="https://images.pexels.com/photos/3564412/pexels-photo-3564412.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                            alt="">
-                    </div>
-                    <div class="chat-details">
-                        <div class="text-head">
-                            <h4>Azariah</h4>
-                            <p class="time">06:49</p>
-                        </div>
-                        <div class="text-message">
-                            <p>“Love, light, laughter.”</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="chat-box">
-                    <div class="img-box">
-                        <img class="img-cover"
-                            src="https://images.pexels.com/photos/2919367/pexels-photo-2919367.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                            alt="">
-                    </div>
-                    <div class="chat-details">
-                        <div class="text-head">
-                            <h4>Oliver</h4>
-                            <p class="time unread">Yesterday</p>
-                        </div>
-                        <div class="text-message">
-                            <p>“Appreciate the mome..”</p>
-                            <b>2</b>
-                        </div>
-                    </div>
-                </div>
-                <div class="chat-box">
-                    <div class="img-box">
-                        <img class="img-cover"
-                            src="https://images.pexels.com/photos/14526673/pexels-photo-14526673.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                            alt="">
-                    </div>
-                    <div class="chat-details">
-                        <div class="text-head">
-                            <h4>Jeslin</h4>
-                            <p class="time">Yesterday</p>
-                        </div>
-                        <div class="text-message">
-                            <p>“Now or never.”</p>
-                        </div>
-                    </div>
-                </div>
+
             </div>
 
         </div>
@@ -200,59 +89,40 @@
                             src="https://images.pexels.com/photos/2474307/pexels-photo-2474307.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                             alt="">
                     </div>
-                    <h4>Leo<br><span>Online</span></h4>
+                    <h4><span id="heading-text">LEO</span><br><span>Online</span></h4>
                 </div>
-                {{-- <div class="nav-icons">
+                <div class="nav-icons">
                     <li><i class="fa-solid fa-magnifying-glass"></i></li>
                     <li><i class="fa-solid fa-ellipsis-vertical"></i></li>
-                </div> --}}
+                </div>
             </div>
 
             <!--chat-container -->
-            <div class="chat-container">
-                <div class="message-box my-message">
-                    <p>I've been waiting to see that show asap!<br><span>07:43</span></p>
-                </div>
-                <div class="message-box friend-message">
-                    <p>Ahh, I can't believe you do too!<br><span>07:45</span></p>
-                </div>
-                <div class="message-box friend-message">
-                    <p>The trailer looks good<br><span>07:45</span></p>
-                </div>
-                <div class="message-box friend-message">
-                    <p>I've been waiting to watch it!!<br><span>07:45</span></p>
-                </div>
-                <div class="message-box my-message">
-                    <p>😐😐😐<br><span>07:46</span></p>
-                </div>
-                <div class="message-box my-message">
-                    <p>Mee too! 😊<br><span>07:46</span></p>
-                </div>
-                <div class="message-box friend-message">
-                    <p>We should video chat to discuss, if you're up for it!<br><span>07:48</span></p>
-                </div>
-                <div class="message-box my-message">
-                    <p>Sure<br><span>07:48</span></p>
-                </div>
-                <div class="message-box my-message">
-                    <p>I'm free now!<br><span>07:48</span></p>
-                </div>
-                <div class="message-box friend-message">
-                    <p>Awesome! I'll start a video chat with you in a few.<br><span>07:49</span></p>
-                </div>
+            <div class="chat-container" style="padding: 0px !important;">
+                <div id="embed_chatbot_container_id" style="height: 700px"></div>
+
             </div>
 
             <!--input-bottom -->
-            <div class="chatbox-input">
-                <input type="file" name="" id="myfile"
-                    style="visibility:hidden;position: absolute;z-index:-10">
-                {{-- <i class="fa-regular fa-face-grin"></i> --}}
-                <label for="myfile"><i class="fa-sharp fa-solid fa-paperclip"></i></label>
-                <input type="text" placeholder="Type a message">
-                {{-- <i class="fa-solid fa-microphone"></i> --}}
-            </div>
+            <!-- <div class="chatbox-input">
+          <i class="fa-regular fa-face-grin"></i>
+          <i class="fa-sharp fa-solid fa-paperclip"></i>
+          <input type="text" placeholder="Type a message">
+          <i class="fa-solid fa-microphone"></i>
+        </div> -->
         </div>
-
+        <script src="https://code.jquery.com/jquery.js"></script>
+        <!-- Bootstrap JavaScript -->
+        <script>
+            $(function() {
+                $(document).on('click', '.chat-box', function() {
+                    var txt = $(this).find('.chat-details').find(".text-head").find('h4').text()
+                    console.log(txt)
+                    $("#heading-text").html(txt)
+                    $(".chat-container").load($.getScript($(this).attr('title')))
+                })
+            })
+        </script>
     </div>
     @include('sweetalert::alert')
 </body>
